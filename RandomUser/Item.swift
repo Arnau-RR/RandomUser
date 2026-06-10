c337a7f4697697af309e19af9c6222a9361e6dff
Arnau Rivas <arnau.rivas21@gmail.com>
2026-06-10 09:02:39 +0200

    Initial Commit

---
+++ RandomUser/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  RandomUser
+//
+//  Created by Arnau on 10/06/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
